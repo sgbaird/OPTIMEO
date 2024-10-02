8e555b72790dd08766b5d5c46b8965251dd5c8b5
colinbousige <colin.bousige@gmail.com>
2024-10-02 15:09:31 +0200

    test

--- pages/3_3._Data_Analysis_and_Modeling.py
+++ pages/3_3._Data_Analysis_and_Modeling.py
@@ -77,7 +77,7 @@ with tabs[0]: # data loading
 
 with tabs[1]: # visual assessment
     if datafile is not None and len(factors) > 0 and len(response) > 0:
-        data[response] = responsevals
+        # data[response] = responsevals
         st.write(data)
         plt.rcParams.update({'font.size': 22})
         ncols = np.min([len(factors),4])
