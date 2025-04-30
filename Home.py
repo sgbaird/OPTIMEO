8f539c7b0c30ccffab2e463f9da3f7225430550b
colinbousige <colin.bousige@gmail.com>
2025-04-30 13:56:17 +0200

    update home.py

--- Home.py
+++ Home.py
@@ -26,29 +26,23 @@ st.markdown("""
 
 ---
 
-- [About this package](#about-this-package)
-- [Documentation](#documentation)
-- [Installation](#installation)
-    - [Installing the package](#installing-the-package)
-    - [Using the web app](#using-the-web-app)
+- [About this app](#about-this-app)
 - [Usage](#usage)
   - [With the web app](#with-the-web-app)
   - [With the Python package](#with-the-python-package)
-    - [For Design of Experiment](#for-design-of-experiment)
-    - [For Bayesian Optimization](#for-bayesian-optimization)
-    - [For Data Analysis](#for-data-analysis)
 - [Support](#support)
 - [How to cite](#how-to-cite)
+- [Documentation](#documentation)
 - [Acknowledgements](#acknowledgements)
 - [License](#license)
 
 ---
 
-## About this package
+## About this app
 
 [OPTIMEO](https://optimeo.streamlit.app/) is a package doubled by a web application that helps you optimize your experimental process by generating a Design of Experiment (DoE), generating new experiments using Bayesian Optimization, and analyzing the results of your experiments using Machine Learning models.
 
-This package was developed within the frame of an academic research project, MOFSONG, funded by the French National Research Agency (N° ANR-24-CE08-7639). See the related paper reference in [How to cite](#how-to-cite).
+This app was developed within the frame of an academic research project, MOFSONG, funded by the French National Research Agency (N° ANR-24-CE08-7639). See the related paper reference in [How to cite](#how-to-cite).
 
 ---
 
@@ -56,7 +50,7 @@ This package was developed within the frame of an academic research project, MOF
 
 ### With the web app
 
-You can use the app directly on its [Streamlit.io web page](https://optimeo.streamlit.app/), or run it locally (see [Installation](#installation)).
+You can use the app directly on its [Streamlit.io web page](https://optimeo.streamlit.app/), or run it locally (see [Installation](https://colinbousige.github.io/OPTIMEO/optimeo.html#installation)).
 
 Choose the page you want to use in the sidebar, and follow the instructions. Hover the mouse on the question marks to get more information about the parameters.
 
@@ -71,114 +65,7 @@ Analyze the results of your experiments and model the response of your process.
 
 ### With the Python package
 
-You can also use the app as a Python package (see [Installation](#installation)). You can import the different modules of the app and use them in your own code. Here is an example of how to use the app as a package:
-
-#### For Design of Experiment
-
-A more detailed example is given in [the notebook](https://github.com/colinbousige/OPTIMEO/blob/main/notebooks/doe.ipynb).
-
-```python
-from optimeo.doe import * 
-parameters = [
-    {'name': 'Temperature', 'type': 'integer', 'values': [20, 40]},
-    {'name': 'Pressure', 'type': 'float', 'values': [1, 2, 3]},
-    {'name': 'Catalyst', 'type': 'categorical', 'values': ['A', 'B', 'C']}
-]
-doe = DesignOfExperiments(
-    type='Sobol sequence',
-    parameters=parameters,
-    Nexp=8
-)
-doe
-```
-
-#### For Bayesian Optimization
-
-A more detailed example is given in [the notebook](https://github.com/colinbousige/OPTIMEO/blob/main/notebooks/bo.ipynb).
-
-```python
-from optimeo.bo import * 
-
-features, outcomes = read_experimental_data('experimental_data.csv', out_pos=[-1])
-bo = BOExperiment(
-    features=features, 
-    outcomes=outcomes,
-    N = 2, # number of new points to generate
-    maximize=True, # we want to maximize the response
-    fixed_features=None, 
-    feature_constraints=None, 
-    optim = 'bo'
-)
-bo.suggest_next_trials()
-```
-
-#### For Data Analysis
-
-A more detailed example is given in [the notebook](https://github.com/colinbousige/OPTIMEO/blob/main/notebooks/MLanalysis.ipynb).
-
-```python
-from optimeo.analysis import * 
-
-data = pd.read_csv('dataML.csv')
-factors = data.columns[:-1]
-response = data.columns[-1]
-analysis = DataAnalysis(data, factors, response)
-analysis.model_type = "ElasticNetCV"
-MLmodel = analysis.compute_ML_model()
-figs = analysis.plot_ML_model()
-for fig in figs:
-    fig.show()
-```
-
----
-
-## Installation
-
-### Installing the package
-
-```bash
-git clone https://github.com/colinbousige/OPTIMEO.git
-cd OPTIMEO
-pip install . # to install OPTIMEO as a package
-```
-
-If you did `pip install .`, you can upgrade to new a version or uninstall with:
-
-```bash
-# upgrade the optimeo package
-cd OPTIMEO
-pip install --upgrade .
-# uninstall the optimeo package
-pip uninstall optimeo
-```
-
-### Using the web app
-
-- You can use the app directly on its [Streamlit.io web page](https://optimeo.streamlit.app/), but it might be a bit slow if you have a lot of data to process. 
-
-- If you'd rather run this app on your local machine (which will most probably make it faster than running it on streamlit.io), you can do so by running the following command in your terminal:
-
-```bash
-git clone https://github.com/colinbousige/OPTIMEO.git
-cd OPTIMEO
-pip install -r requirements.txt # to install the required packages
-```
-
-Finally, you can run the app by running the following command in your terminal:
-
-```bash
-streamlit run Home.py
-```
-
-- You can also modify the path to the `OPTIMEO` folder in `OPTIMEO/bin/optimeo`. Then, doing the following will add the `optimeo` command to your `PATH`:
-
-```bash
-git clone https://github.com/colinbousige/OPTIMEO.git
-cd OPTIMEO
-pip install . # to install OPTIMEO as a package
-```
-
-So now, you just have to run `optimeo` in your terminal to run the app.
+You can also use the app as a Python package (see [Documentation](https://colinbousige.github.io/OPTIMEO/optimeo.html).
 
 ---
 
